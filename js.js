8a47bcd0687913783bdcae4101ccc072cfc38053
Elbay Malik <eleo.666@gmail.com>
2023-05-19 19:23:40 +0200

    Create js.js
    Add logic for changing background to red green and blue

---
+++ js.js
@@ -0,0 +1,11 @@
+document.getElementById('red').addEventListener('click', function () {
+  document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('green').addEventListener('click', function () {
+  document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('blue').addEventListener('click', function () {
+  document.body.style.backgroundColor = 'blue';
+});
